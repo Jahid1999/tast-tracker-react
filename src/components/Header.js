17af8d3e38cad9669055f642fe3ef5cea09ca163
Jahid1999 <bsse1030@iit.du.ac.bd>
2022-01-06 00:12:57 +0600

    added header

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import Button from './Button'
+function Header({title}) {
+    return (
+      <header className="header">
+          <h1>{title}</h1>
+          <Button color='green' text='Add' />
+      </header>
+    );
+  }
+
+  Header.defaultProps = {
+      title: 'Task Tracker'
+  }
+  
+  export default Header;
