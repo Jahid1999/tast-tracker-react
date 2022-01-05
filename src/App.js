22f0dca465b42b2cd7f3a3670e82982c9783a7f3
Jahid1999 <bsse1030@iit.du.ac.bd>
2022-01-06 01:04:26 +0600

    task delete added

--- src/App.js
+++ src/App.js
@@ -23,10 +23,20 @@ function App() {
         reminder: false,
     },
 ])
+
+//Delete Task
+
+const deleteTask = (id) => {
+//  console.log(id);
+    setTasks(tasks.filter((task) => task.id !== id))
+} 
   return (
     <div className="container">
       <Header />
-      <Tasks  tasks={tasks} />
+      { tasks.length > 0 ? <Tasks  tasks={tasks} onDelete = {deleteTask} /> : 'No Tasks Left'
+
+      }
+      
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,10 @@
 import { FaTimes } from "react-icons/fa";
-function Task({task}) {
+function Task({task, onDelete}) {
    
     return (
       <div className="task">
-          <h3> {task.text} <FaTimes style={{color:'red'}} /></h3>
+          <h3> {task.text} <FaTimes style={{color:'red', cursor:'pointer'}} 
+          onClick={() => {onDelete(task.id)}} /></h3>
           <p> {task.date}</p>
       </div>
     );
