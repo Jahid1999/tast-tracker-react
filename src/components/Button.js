3cbdbf9d95f25d16bcae0d457a629a84b6fc1c70
Jahid1999 <bsse1030@iit.du.ac.bd>
2022-01-10 21:03:12 +0600

    routing test completed

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,4 @@
 function Button({color, text, onClick}) {
-    // const onClick= () => {
-    //     console.log('clicked')
-    // }
     return (
       <button style={{backgroundColor: color}} className="btn" onClick={onClick}>
           {text}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,19 @@
 import Button from './Button'
+import {useLocation} from 'react-router-dom'
 function Header({title, onAddClick, showAdd}) {
      const onClick= () => {
         console.log('clicked')
     }
+    const location = useLocation();
     return (
       <header className="header">
           <h1>{title}</h1>
-          {
-            onAddClick && <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} 
+          { 
+            location.pathname === '/' && (
+              onAddClick && <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} 
             onClick={onAddClick}/>
+            )
+            
           }
           
       </header>
